690712c5c4dc9efd16a58370aa7587a952cbdbb1
pavel.pesetsky <pavel.pesetsky@pwc.com>
2022-11-27 19:11:05 +0400

    Add project files.

---
+++ AlertProcessing/SensorReading.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlertProcessing
+{
+    internal class SensorReading
+    {
+        public Machine machine { get; set; }
+        public Ambient ambient { get; set; }
+        public DateTime timeCreated { get; set; }
+    }
+
+    public class Machine
+    {
+        public double temperature { get; set; }
+        public double pressure { get; set; }
+
+    }
+    public class Ambient
+    {
+        public double temperature { get; set; }
+        public int humidity { get; set; }
+
+    }
+}
